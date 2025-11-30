3179b88d915b78fc9471c8a5e6a38c33a34583ca
hversu <keegankeplinger@gmail.com>
2025-11-29 16:53:38 -0900

    add latest job

--- refresh_jobs.sh
+++ refresh_jobs.sh
@@ -3,7 +3,7 @@
 
   curl -X POST http://localhost:5000/api/jobs -H "Content-Type:application/json" -d @api/jobs/chaos_treasury.json
   curl -X POST http://localhost:5000/api/jobs -H "Content-Type:application/json" -d @api/jobs/agent_of_chaos.json
-
+  curl -X POST http://localhost:5000/api/jobs -H "Content-Type:application/json" -d @api/jobs/deep_dive.json
   # Restart and watch the new logging
   docker-compose restart api
   docker logs -f crawling-chaos-api

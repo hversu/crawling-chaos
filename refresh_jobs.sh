454ce297f7c0fb4697fd913ccc5744c897567f43
hversu <keegankeplinger@gmail.com>
2025-11-29 16:58:15 -0900

    docker hotfix

--- refresh_jobs.sh
+++ refresh_jobs.sh
@@ -5,5 +5,5 @@
   curl -X POST http://localhost:5000/api/jobs -H "Content-Type:application/json" -d @api/jobs/agent_of_chaos.json
   curl -X POST http://localhost:5000/api/jobs -H "Content-Type:application/json" -d @api/jobs/deep_dive.json
   # Restart and watch the new logging
-  docker-compose restart api
+  docker compose restart api
   docker logs -f crawling-chaos-api
